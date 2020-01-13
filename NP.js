51ab69496efafa64a4113e878e5be77c486972e3
PapyrusCompendium <51677630+PapyrusCompendium@users.noreply.github.com>
2020-01-13 15:53:22 -0500

    Update NP.js

--- NP.js
+++ NP.js
@@ -1,11 +1,14 @@
-const $natoPhonetics = ["Alfa","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliett","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","X-ray","Yankee","Zulu", " ",  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", ".", "!", "@", "#", "$", "%", "^", "&", "(", ")", "_", "=", "|", ";", ":", "<", ">", "?", "/", "`"]
+const $natoPhonetics = ["Alfa","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliett","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","X-ray","Yankee","Zulu", " ",  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", ".", "!", "@", "#", "$", "%", "^", "&", "(", ")", "_", "=", "|", ";", ":", "<", ">", "?", "/", "`"];
 
 $("#message").on('change keydown paste input', function(){
 
-    $result = ""
+    $result = "";
     $("#message").val().split('').forEach(function(item)
     {
-        $result += `${$natoPhonetics.find(element => element.startsWith(item.toUpperCase()))} `
+        if(item != " ")
+            $result += `${$natoPhonetics.find(element => element.startsWith(item.toUpperCase()))} `;
+        else
+            $result += "|| ";
     });
     $("#output").text($result);
 });
