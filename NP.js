d7ec1f09c3156e44fa989d1669646250f885efd6
PapyrusCompendium <51677630+PapyrusCompendium@users.noreply.github.com>
2020-01-14 07:40:15 -0500

    Update NP.js

--- NP.js
+++ NP.js
@@ -1,7 +1,6 @@
 const $natoPhonetics = ["Alfa","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliett","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","X-ray","Yankee","Zulu", " ",  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", ".", "!", "@", "#", "$", "%", "^", "&", "(", ")", "_", "=", "|", ";", ":", "<", ">", "?", "/", "`"];
 
 $("#message").on('change keydown paste input', function(){
-
     $result = "";
     $("#message").val().split('').forEach(function(item)
     {
